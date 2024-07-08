3b02d80dc73a487ec0c328b685f93b390fcbb243
pashu3 <pashupathimali03@gmail.com>
2024-07-08 13:39:46 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3001;
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://pashu-portfolio.netlify.app/',
+  origin: 'https://pashu-portfolio.netlify.app',
 }));
 
 app.use(morgan('dev'));
